2a652969769afcab64a124377c5cf2cf4cec4af9
moshiko1988 <moshiko1988@gmail.com>
2017-02-26 19:31:57 -0500

    deploy task

---
+++ public/application.js
@@ -0,0 +1,19 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports) {
+
+	// 'use strict';
+
+	// user require with a reference to bundle the file and use it in this file
+	// var example = require('./example');
+
+	// load manifests
+	// scripts
+	// require('./assets/scripts/index.js');
+
+	// styles
+	// require('./assets/styles/index.scss');
+	"use strict";
+
+/***/ }
+]);
